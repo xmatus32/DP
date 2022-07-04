196afc7fdd111945105e02de6551a2f553b56953
xmatus32 <xmatus32@vutbr.cz>
2022-07-04 13:52:20 +0000

    docker compose pre sigmac service

--- microservices/sigmacService/app.py
+++ microservices/sigmacService/app.py
@@ -8,13 +8,15 @@ import configparser
 
 app = Flask(__name__)
 Config = configparser.ConfigParser()
-configFilePath = './config.ini'
+configFilePath = './microservices/sigmacService/config.ini'
 Config.read(configFilePath)
 
 @app.route("/lists")
 def get_list():
+    os.chdir( Config.get('Paths', 'Tools'))
+
     target = '--lists'
-    sigmac = "sigmac {}".format(target)
+    sigmac = "python3 sigmac {}".format(target)
     return subprocess.getoutput(sigmac) #sigmac --lists
 
 @app.route("/", methods=['POST'])
@@ -31,4 +33,10 @@ def convert():
     sigmac = "python3 sigmac -t {} -c {} ./rule.yaml".format(data['target'], data['config']) #python3 sigmac -t splunk -c splunk-windows ../rules/windows/driver_load/driver_load_mal_creddumper.yml
     return subprocess.getoutput(sigmac)
 
-                                                
+@app.route("/hello")
+def hello():
+    os.chdir( Config.get('Paths', 'Tools'))
+    return os. getcwd() 
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)                            
